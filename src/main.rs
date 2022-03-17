6cc26c1a3175cd236265670790dcd41381e4a365
Andrii Honda <andriy.gonda@gmail.com>
2022-03-17 22:22:46 +0200

    added random useragents choose

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,21 @@
-// use log::info;
-// use log::warn;
-// use threadpool::ThreadPool;
-// use env_logger::Env;
-// use std::sync::{Barrier, Arc};
 mod provider;
-// fn fetch(url: &str) -> Result<u16, isahc::Error> {
-//     let response = isahc::get(url)?;
-//     Ok(response.status().as_u16())
-// }
-use provider::Provider;
+mod useragent;
+
 fn main() {
-    let proxies = Provider::load_proxies_from_url(
-        "https://raw1.githubusercontent.com/opengs/uashieldtargets/v2/proxy.json",
-    )
-    .unwrap();
-    for proxy in proxies {
-        println!("{:?}", proxy)
+    // let proxies = provider::load_proxies_from_url(
+    //     "https://raw.githubusercontent.com/opengs/uashieldtargets/v2/proxy.json",
+    // )
+    // .unwrap();
+    // for proxy in proxies {
+    //     println!("{:?}", proxy)
+    // }
+    for _ in 1..10 {
+        let agent = useragent::random_agent();
+        println!("{:?}", agent);
     }
-    // let proxy = Proxy{ip: "127.0.0.1".to_string(), scheme: "http".to_string()};
-    // let serialized = serde_json::to_string(&proxy).unwrap();
-    // println!("serialized = {}", serialized);
-    // env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    // let workers_count = 500;
-    // let pool = ThreadPool::new(workers_count);
-    // let n_jobs = 100;
-    // let barrier = Arc::new(Barrier::new(n_jobs + 1));
-    // // let an_atomic = Arc::new(AtomicUsize::new(0));
-
-    // for _ in 0..n_jobs {
-    //     let barrier = barrier.clone();
-    //     pool.execute(move|| {
-    //         match fetch("https://lenta.ru") {
-    //             Ok(code) => info!("Site responses with code {}", code),
-    //             Err(_) => warn!("Unable to make request to site.")
-    //         }
-
-    //         barrier.wait();
-    //     });
+   
+    // let sites = provider::load_sites_from_url("https://raw.githubusercontent.com/opengs/uashieldtargets/v2/sites.json").unwrap();
+    // for site in sites {
+    //     println!("{:?}", site);
     // }
-
-    // // barrier.wait();
-    // pool.join();// env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    // let workers_count = 500;
-    // let pool = ThreadPool::new(workers_count);
-    // let n_jobs = 100;
-    // let barrier = Arc::new(Barrier::new(n_jobs + 1));
-    // // let an_atomic = Arc::new(AtomicUsize::new(0));
-
-    // for _ in 0..n_jobs {
-    //     let barrier = barrier.clone();
-    //     pool.execute(move|| {
-    //         match fetch("https://lenta.ru") {
-    //             Ok(code) => info!("Site responses with code {}", code),
-    //             Err(_) => warn!("Unable to make request to site.")
-    //         }
-
-    //         barrier.wait();
-    //     });
-    // }
-
-    // // barrier.wait();
-    // pool.join();
 }
--- src/provider/mod.rs
+++ src/provider/mod.rs
@@ -1,14 +1,20 @@
 use isahc::prelude::*;
 pub mod proxy;
+pub mod site;
 use proxy::Proxy;
+use site::Site;
 
-pub struct Provider {}
 
-impl Provider {
-    pub fn load_proxies_from_url(url: &str) -> Result<Vec<Proxy>, Box<dyn std::error::Error>> {
-        let mut response = isahc::get(url)?;
-        let content = response.text()?;
-        let proxies: Vec<Proxy> = serde_json::from_str(content.as_str())?;
-        Ok(proxies)
-    }
+pub fn load_proxies_from_url(url: &str) -> Result<Vec<Proxy>, Box<dyn std::error::Error>> {
+    let mut response = isahc::get(url)?;
+    let content = response.text()?;
+    let proxies: Vec<Proxy> = serde_json::from_str(content.as_str())?;
+    Ok(proxies)
+}
+
+pub fn load_sites_from_url(url: &str) -> Result<Vec<Site>, Box<dyn std::error::Error>> {
+    let mut response = isahc::get(url)?;
+    let content = response.text()?;
+    let sites: Vec<Site> = serde_json::from_str(content.as_str())?;
+    Ok(sites)
 }
